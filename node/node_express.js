e314c705b3ffb3dc12cc5ba5fc16ebfbed81cf28
Gabi Teaching <gabiteaching@Gabi-Air.local>
2016-07-17 14:47:30 -0400

    Added car price example

--- node/node_express.js
+++ node/node_express.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/goodbye', function (req, res) {
+  res.send('Goodbye World!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
