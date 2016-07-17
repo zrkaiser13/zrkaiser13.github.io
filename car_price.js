0513c7113938f7ed00a312a03743cd3d6cb25cc0
Gabi Teaching <gabiteaching@Gabi-Air.local>
2016-07-17 14:59:25 -0400

    cleaning up examples.

--- car_price.js
+++ car_price.js
@@ -3,11 +3,6 @@ $(document).ready(function(){
 	$("#set-price-submit").click(function(e){
 		e.preventDefault();
 
-		var data = {};
-		data.title = "title";
-		data.message = "message";
-
-		console.log($('#car-price').val())
         $.ajax({
             url: "http://localhost:3000/set_price",
             type: "GET",
